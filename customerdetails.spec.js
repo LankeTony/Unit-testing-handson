c19a892d6ab6f97b0e03ecfcc4f21bbde0cfb20b
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2022-01-21 17:43:41 +0530

    basic details test suite created

--- customerdetails.spec.js
+++ customerdetails.spec.js
@@ -1,3 +1,15 @@
-describe('customer-details.js', function () {});
+describe('Basicdetails.js', function () {
+  it('should be greater than 10000', function () {
+    const amount = new Amount();
+    amount.total = 10000;
 
-describe('basic-details.js', function () {});
+    expect(amount.total).toBe(10000);
+  });
+
+  it('should not be greater than 100000', function () {
+    const amount = new Amount();
+    amount.total = 10000;
+
+    expect(amount.total).not.toBe(10000);
+  });
+});
